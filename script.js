07a03b1141ccb1f5dad9f8b216770e5c91286a93
miwio <dark_rinoa89@hotmail.com>
2021-03-23 12:21:56 +0800

    Answers

---
+++ script.js
@@ -0,0 +1,47 @@
+function showName(){
+    var x = document.getElementById("fullName");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+
+};
+
+function showEmail(){
+    var x = document.getElementById("email");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    
+};
+
+function showPhone(){
+    var x = document.getElementById("phone");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+
+};
+
+function showTelegram(){
+    var x = document.getElementById("telegram");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    
+};
+
+function changeBG1(){
+    document.body.style.backgroundColor = "#FF9900"
+};
+
+function changeBG2(){
+    document.body.style.backgroundColor = "#7B2CF1"
+};
